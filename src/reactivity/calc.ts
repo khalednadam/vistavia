dbab0a61a1f5d76b774a77f907816d9a16722283
Khaled Nadam <khalednadam@gmail.com>
2025-10-16 20:59:18 +0300

    feat(calc): add calc function for reactive calculations in Vistavia reactivity system

---
+++ src/reactivity/calc.ts
@@ -0,0 +1,23 @@
+/** 
+ * @fileoverview Provides the calc function for the vistavia reactivity system.
+ * 
+ * The calc function is a wrapper around the via function that allows for reactive calculations.
+ * 
+ * @author Khaled
+ * @created 2025-10-16
+ */
+
+import { effect } from "./effect.ts";
+import { via } from "./via.ts";
+
+export const calc = (fn: () => any) => {
+  const res = via(fn());
+  effect(() => {
+    res.v = fn();
+  });
+  return {
+    get v() {
+      return res.v;
+    },
+  };
+};
