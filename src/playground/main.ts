baf18c07819f872dd973d0dec1a7094653b1cca1
Khaled Nadam <khalednadam@gmail.com>
2025-10-17 17:40:45 +0300

    feat(playground): enhance main.ts with mesh and effect for item management

--- src/playground/main.ts
+++ src/playground/main.ts
@@ -1,10 +1,18 @@
-import { calc, via } from '../index.ts' 
+import { calc, effect, mesh, via } from '../index.ts' 
 
 const count = via(4);
 const doubleCount = calc(() => count.v * 2);
 
+const item = mesh({ name: 'item', price: 10 });
+
 const tripleCount = calc(() => doubleCount.v * 3);
 
+effect(() => {
+    console.log('item', item)
+})
 
-console.log('doubleCount', doubleCount.v)
-console.log('tripleCount', tripleCount.v)
+// console.log('item', item.price)
+// console.log('doubleCount', doubleCount.v)
+item.price = 20;
+
+// console.log('tripleCount', tripleCount.v)
