86fd4effe0c80dcadba6ad4efd027f96462cb3da
Khaled Nadam <khalednadam@gmail.com>
2025-10-25 11:49:36 +0300

    feat(bindings): add bindEvent function for event handling in DOM bindings

--- src/dom/bindings.ts
+++ src/dom/bindings.ts
@@ -1,7 +1,7 @@
 /** @fileoverview Provides the binding functions for the vistavia dom bindings system.
- * 
+ *
  * The binding functions are used to bind reactive data to the DOM.
- * 
+ *
  * @author Khaled Nadam
  * @created 2025-10-19
  */
@@ -17,3 +17,37 @@ export const bindText = (node: HTMLElement, getter: () => any) => {
     node = null as unknown as HTMLElement;
   };
 };
+
+export const bindEvent = (
+  element: HTMLElement,
+  eventName: string,
+  handler: () => any
+) => {
+  let currentHandler = handler();
+
+  const wrapper = (event: Event) => {
+    if (!currentHandler) return;
+
+    if (typeof currentHandler === "function") {
+      currentHandler(event);
+    } else {
+      currentHandler.handleEvent(event);
+    }
+  };
+
+  element.addEventListener(eventName, wrapper);
+
+  const eff = effect(() => {
+    currentHandler = handler();
+  });
+
+  return () => {
+    eff.stop();
+    element.removeEventListener(eventName, wrapper);
+  };
+};
+
+/**
+ * TODO: Bind Attributes
+ * TODO: Bind Styles
+ */
